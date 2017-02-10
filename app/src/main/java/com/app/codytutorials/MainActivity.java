ef71f34e988d3e7f175cb695b7206533935cf529
Evgeniy <badula4@gmail.com>
2017-02-10 20:24:32 +0300

    Настройка дизайна меню и главной страницы

--- app/src/main/java/com/app/codytutorials/MainActivity.java
+++ app/src/main/java/com/app/codytutorials/MainActivity.java
@@ -80,13 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_java) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_javaScript) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_python) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_web) {
 
         } else if (id == R.id.nav_share) {
 
