3f33cdab234f295eba606cb8602978c077239162
Evgeniy <badula4@gmail.com>
2017-02-11 13:55:43 +0300

    Добавление нового активити с табами

--- app/src/main/java/com/app/codytutorials/MainActivity.java
+++ app/src/main/java/com/app/codytutorials/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.codytutorials;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -79,9 +80,11 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Intent intent;
 
         if (id == R.id.nav_java) {
-            // Handle the camera action
+            intent = new Intent(this, ActivityTabs.class);
+            startActivity(intent);
         } else if (id == R.id.nav_javaScript) {
 
         } else if (id == R.id.nav_python) {
