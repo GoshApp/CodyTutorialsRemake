12b46fbe6b6df9234fc05ee0450b0d33047a7490
Rikmen <rikmenv@yandex.ru>
2017-02-12 15:21:00 +0300

    Добавил адаптер для подключения фрагментов. Внес изминения в табы, теперь они работают коректно + подгружать можно нужный вариант.

--- app/src/main/java/com/app/codytutorials/ActivityTabs.java
+++ app/src/main/java/com/app/codytutorials/ActivityTabs.java
@@ -2,33 +2,58 @@ package com.app.codytutorials;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
-import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 
-public class ActivityTabs extends Activity implements ActionBar.TabListener {
-
-    final String LOG_TAG = "myLogs";
+public class ActivityTabs extends FragmentActivity implements ActionBar.TabListener {
+    private ViewPager viewPager;
+    final String LOG_TAG = "myLogs"; // добавлем логи
+    private ActionBar actionBar;
+    private AdapterProfile tabPagerAdapter;
+    private String[] tabs = { "Плейлист", "Методичка" };
+    // массив для табов
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tabs);
+        setContentView(R.layout.swipetab); // подгружаем хml для табов
 
-        ActionBar bar = getActionBar();
 
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        tabPagerAdapter = new AdapterProfile(getSupportFragmentManager());
+        viewPager.setAdapter(tabPagerAdapter);
 
-        Tab tab = bar.newTab();
-        tab.setText("tab1");
-        tab.setTabListener(this);
-        bar.addTab(tab);
+        actionBar = getActionBar();
+        actionBar.setTitle("Как будем учиться"); // заголовок
 
-        tab = bar.newTab();
-        tab.setText("tab2");
-        tab.setTabListener(this);
-        bar.addTab(tab);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        for (String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            /**
+             * on swipe select the respective tab
+             * */
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+                // в зависимости от позиции
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) { }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) { }
+        });
 
     }
 
@@ -39,6 +64,8 @@ public class ActivityTabs extends Activity implements ActionBar.TabListener {
 
     @Override
     public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        // при переходе на новый таб, присваеваем  pos чтобы обновить даннвые
+        viewPager.setCurrentItem(tab.getPosition());
         Log.d(LOG_TAG, "selected tab: " + tab.getText());
     }
 
